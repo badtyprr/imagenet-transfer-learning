bfdac8dc88525697cc2aea1defeb1aadd1729894
Simeon Trieu <strieu@intevac.com>
2018-11-20 16:56:46 -0800

    set download directory

--- download.sh
+++ download.sh
@@ -4,6 +4,6 @@ if [ $# -ne 2 ]; then
   exit 1
 fi
 
-wget $2 -O $1 -T 1 -t 5 -nc -b -a wget.log
+wget $2 -O $1 -T 1 -t 5 -nc -b -a wget.log -P /home/strieu/t_drive/temp/simeon/dataset/imagenet/
 # Downloads ImageNet in about 4 days
 sleep 0.25
